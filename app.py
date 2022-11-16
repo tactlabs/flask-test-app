72a0e54a3d20a3ab57df2a3175f6967220f181ee
vijitha9129 <vijitha.kanumuru@gmail.com>
2022-11-15 22:48:06 -0600

    added ramdom number generation

--- app.py
+++ app.py
@@ -12,14 +12,23 @@ Source:
 
 from flask import Flask, render_template
 import json
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def start():
+    random_num()
+    random_result = random_num()
+    return render_template('index.html',result=random_result)
 
-    return render_template('index.html')
+    
 
+def random_num():
+    random_number=random.randint(0,100)
+    return random_number
 
 if __name__== "__main__":
     app.run(host="0.0.0.0", debug = True, port = 8000)
+    
+    
