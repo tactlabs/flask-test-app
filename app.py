230dd341759aeaca35b0c8edc348dac5b9c34409
vijitha9129 <vijitha.kanumuru@gmail.com>
2022-11-16 18:22:39 -0600

    business logic isolated

--- app.py
+++ app.py
@@ -12,21 +12,15 @@ Source:
 
 from flask import Flask, render_template
 import json
-import random
+import business
 
 app = Flask(__name__)
 
 @app.route('/')
 def start():
-    random_num()
-    random_result = random_num()
+    random_result = business.random_num()
     return render_template('index.html',result=random_result)
-
     
-
-def random_num():
-    random_number=random.randint(0,100)
-    return random_number
 
 if __name__== "__main__":
     app.run(host="0.0.0.0", debug = True, port = 8000)
---
+++ business.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 20, 2022
+
+Course work: 
+
+@author: Sivaraam
+
+Source:
+    
+'''
+
+import random
+
+def random_num():
+    random_number=random.randint(0,100)
+    return random_number
